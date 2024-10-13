e5890c298bc522d2a5d132a433a4756a6b9ee445
Muhammed4101 <muhammedarslangs41@gmail.com>
2024-10-13 14:27:25 +0300

    Add files via upload

---
+++ 42/ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 13:14:05 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/13 13:14:05 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		if (*(unsigned char *)(s + i) == (unsigned char)c)
+			return ((void *)(s + i));
+		i++;
+	}
+	return (0);
+}
