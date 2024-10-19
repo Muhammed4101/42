a9b795ed94e818fccf5ac4b8ca0b8deeaf2a950d
Muhammed4101 <muhammedarslangs41@gmail.com>
2024-10-19 09:09:37 +0300

    Add files via upload

---
+++ libft.a/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 13:59:03 by marvin            #+#    #+#             */
+/*   Updated: 2024/10/13 13:59:03 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(count * size);
+	if (!ptr)
+		return (0);
+	ft_bzero(ptr, count);
+	return (ptr);
+}
